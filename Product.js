2e6fb86508d2c5648dda3038c378dec5afea6b61
Diego Moscoso <moscosovargasdiego@gmail.com>
2025-06-04 12:05:39 -0500

    Crear archivo Product.js

---
+++ Product.js
@@ -0,0 +1,29 @@
+// Product.js
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: false,
+  },
+  imageUrl: {
+    type: String,
+    required: false,
+  },
+}, {
+  timestamps: true, // createdAt, updatedAt
+});
+
+module.exports = mongoose.model('Product', productSchema);
